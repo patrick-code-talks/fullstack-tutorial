d6b2271d1760d8630df82c38f2315efbd866aa01
patrick-code-talks <patrickcodes1995@gmail.com>
2022-11-12 17:39:54 -0800

    schema builded

---
+++ start/server/src/resolvers.js
@@ -0,0 +1,37 @@
+module.exports = {
+  Query: {
+    launches: (_, __, { dataSources }) => dataSources.launchAPI.getAllLaunches(),
+    launch: (_, { id }, { dataSources }) =>
+      dataSources.launchAPI.getLaunchById({ launchId: id }),
+    me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
+  },
+  // 返回Mission的missionPatch attribute
+  Mission: {
+    // The default size is 'LARGE' if not provided
+    missionPatch: (mission, { size } = { size: 'LARGE' }) => {
+      return size === 'SMALL'
+        ? mission.missionPatchSmall
+        : mission.missionPatchLarge;
+    },
+  },
+  Launch: {
+    // isBooked是Launch的attribute，所以， parent 为launch，args为空
+    isBooked: async (launch, _, { dataSources }) =>
+      dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
+  },
+  User: {
+    trips: async (_, __, { dataSources }) => {
+      // get ids of launches by user
+      const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
+  
+      if (!launchIds.length) return [];
+  
+      // look up those launches by their ids
+      return (
+        dataSources.launchAPI.getLaunchesByIds({
+          launchIds,
+        }) || []
+      );
+    },
+  },
+};
